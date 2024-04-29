456dc518bdae6da3adf48b290b9fbd83bc370f25
nnavidd <navid.nabaeei@gmail.com>
2024-04-30 01:04:06 +0200

    Module01 ex04 done

---
+++ Module_01/ex04/src/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nnabaeei <nnabaeei@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/29 11:52:13 by nnabaeei          #+#    #+#             */
+/*   Updated: 2024/04/30 01:03:09 by nnabaeei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	std::string		text = "";
+	size_t			pos;
+
+	if (ac == 4)
+	{
+		std::ifstream	in(av[1]);
+		if (!in.is_open())
+			return (std::cout << "Opening the source is not possible!" << std::endl, 1);
+		
+		std::getline(in, text, '\0');
+		
+		while ((pos = text.find(av[2])) != std::string::npos)
+		{
+			text.erase(pos, std::string(av[2]).length());
+			text.insert(pos, av[3]);
+		}
+		
+		std::ofstream	ou((std::string(av[1]) + ".replace").c_str());
+		if (!ou.is_open())
+			return (std::cout << "Opening the destinaiton is not possible!!" << std::endl, 1);
+		
+		ou << text;
+		
+		in.close();
+		ou.close();
+	}
+	return (0);
+}
