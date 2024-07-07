7dde18bc98c2310afe3fa8dcdedad58e3bcfa943
nnavidd <navid.nabaeei@gmail.com>
2024-07-07 11:48:16 +0200

    Final commit: Release version 1.0.0 Module09 ex00

--- Module_09/ex00/src/BitcoinExchange.cpp
+++ Module_09/ex00/src/BitcoinExchange.cpp
@@ -3,70 +3,69 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.cpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nnabaeei <nnabaeei@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: nnavidd <nnavidd@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/06 13:46:50 by nnabaeei          #+#    #+#             */
-/*   Updated: 2024/07/06 13:47:37 by nnabaeei         ###   ########.fr       */
+/*   Updated: 2024/07/07 11:15:38 by nnavidd          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/BitcoinExchange.hpp"
 
-// Default constructor
 BTC::BTC() {}
 
-// Constructor with file path
-BTC::BTC(const std::string& dbFilePath) {
-    if (!loadDatabase(dbFilePath)) {
-        throw std::runtime_error("Error: could not open file.");
-    }
+BTC::BTC( std::string const & dbFilePath ) {
+	if (!loadDatabase(dbFilePath)) {
+		throw std::runtime_error( RED "Error: " ORG "could not open file." RESET);
+	}
 }
 
-// Copy constructor
-BTC::BTC(const BTC& other) : btcRates(other.btcRates) {}
+BTC::BTC( BTC const & other ) : btcRates(other.btcRates) {}
 
-// Copy assignment operator
-BTC& BTC::operator=(const BTC& other) {
-    if (this != &other) {
-        btcRates = other.btcRates;
-    }
-    return *this;
+BTC & BTC::operator=( BTC const & other) {
+	if (this != &other) {
+		btcRates = other.btcRates;
+	}
+	return (*this);
 }
 
-// Destructor
 BTC::~BTC() {}
 
 // Private method to load the database from a file
-bool BTC::loadDatabase(const std::string& dbFilePath) {
-    std::ifstream dbFile(dbFilePath.c_str());
-    if (!dbFile.is_open()) return false;
+bool BTC::loadDatabase( std::string const & dbFilePath ) {
+	std::string		line;
+	std::string		date;
+	float 			rate;
+	std::ifstream	dbFile;
 
-    std::string line, date;
-    float rate;
-    while (std::getline(dbFile, line)) {
-        std::istringstream ss(line);
-        if (std::getline(ss, date, ',') && ss >> rate) {
-            btcRates[date] = rate;
-        }
-    }
-    return true;
+	dbFile.open(dbFilePath.c_str());
+	if (!dbFile.is_open())
+		throw std::runtime_error( RED "Error: " ORG "could not open db file." RESET);
+
+	while (std::getline(dbFile, line)) {
+		std::istringstream ss(line);
+		if (std::getline(ss, date, ',') && ss >> rate) {
+			btcRates[date] = rate;
+		}
+	}
+	return (true);
 }
 
 // Private method to find the closest date in the map
-std::string BTC::findClosestDate(const std::string& date) const {
-    std::map<std::string, float>::const_iterator it = btcRates.lower_bound(date);
-    if (it == btcRates.begin() || (it != btcRates.end() && it->first != date)) {
-        --it;
-    }
-    return it->first;
+std::string BTC::findClosestDate( std::string const & date ) const {
+	std::map<std::string, float>::const_iterator it = btcRates.lower_bound(date);
+	if (it == btcRates.begin() || (it != btcRates.end() && it->first != date)) {
+		--it;
+	}
+	return (it->first);
 }
 
-// Method to get the rate for a specific date
-float BTC::getRate(const std::string& date) const {
-    std::map<std::string, float>::const_iterator it = btcRates.find(date);
-    if (it == btcRates.end()) {
-        std::string closestDate = findClosestDate(date);
-        return btcRates.at(closestDate);
-    }
-    return it->second;
+// Method to get the rate for a specific dat
+float BTC::getRate( std::string const & date ) const {
+	std::map<std::string, float>::const_iterator it = btcRates.find(date);
+	if (it == btcRates.end()) {
+		std::string closestDate = findClosestDate(date);
+		return (btcRates.at(closestDate));
+	}
+	return (it->second);
 }
