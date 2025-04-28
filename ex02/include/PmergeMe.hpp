39cd5c9cc5186a6e8eed636cb5aefb9c32d2c2cc
nnavidd <navid.nabaeei@gmail.com>
2025-04-28 16:36:00 +0200

    working on the Module_09 ex02 the Ford Johnson algorithm

---
+++ ex02/include/PmergeMe.hpp
@@ -0,0 +1,83 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <vector>
+#include <list>
+#include <deque>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+
+
+class PmergeMe {
+private:
+    std::vector<int> _vecData;
+    std::list<int> _listData;
+
+public:
+    PmergeMe();
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+
+    void loadInput(int argc, char* argv[]);
+    void sortVector();
+    void sortList();
+
+    const std::vector<int>& getVectorData() const;
+    const std::list<int>& getListData() const;
+
+    template <typename T>
+    void fordJohnsonSort(T& container);
+
+private:
+    template <typename T>
+    void insertIntoSorted(T& sortedContainer, typename T::value_type element);
+};
+
+// Template Definitions
+
+template <typename T>
+void PmergeMe::fordJohnsonSort(T& container) {
+    if (container.size() <= 1) return;
+
+    T largerElements, smallerElements;
+
+    // Pair elements and divide into two groups
+    auto it = container.begin();
+    while (it != container.end()) {
+        auto next = std::next(it);
+        if (next == container.end()) {
+            largerElements.push_back(*it);
+            break;
+        }
+
+        if (*it < *next) {
+            smallerElements.push_back(*it);
+            largerElements.push_back(*next);
+        } else {
+            smallerElements.push_back(*next);
+            largerElements.push_back(*it);
+        }
+        std::advance(it, 2);
+    }
+
+    // Recursively sort the larger group
+    fordJohnsonSort(largerElements);
+
+    // Insert smaller elements into the sorted larger group
+    for (const auto& element : smallerElements) {
+        insertIntoSorted(largerElements, element);
+    }
+
+    // Replace the original container with the sorted result
+    container = largerElements;
+}
+
+template <typename T>
+void PmergeMe::insertIntoSorted(T& sortedContainer, typename T::value_type element) {
+    auto position = std::lower_bound(sortedContainer.begin(), sortedContainer.end(), element);
+    sortedContainer.insert(position, element);
+}
+
+#endif 
---
+++ ex02/src/PmergeMe.cpp
@@ -0,0 +1,43 @@
+#include "PmergeMe.hpp"
+
+PmergeMe::PmergeMe() {}
+
+PmergeMe::PmergeMe(const PmergeMe& other)
+    : _vecData(other._vecData), _listData(other._listData) {}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other) {
+    if (this != &other) {
+        _vecData = other._vecData;
+        _listData = other._listData;
+    }
+    return *this;
+}
+
+PmergeMe::~PmergeMe() {}
+
+const std::vector<int>& PmergeMe::getVectorData() const {
+    return _vecData;
+}
+
+const std::list<int>& PmergeMe::getListData() const {
+    return _listData;
+}
+
+void PmergeMe::loadInput(int argc, char* argv[]) {
+    for (int i = 1; i < argc; ++i) {
+        int value = std::stoi(argv[i]);
+        if (value < 0) {
+            throw std::invalid_argument("Error: Negative numbers are not allowed.");
+        }
+        _vecData.push_back(value);
+        _listData.push_back(value);
+    }
+}
+
+void PmergeMe::sortVector() {
+    fordJohnsonSort(_vecData);
+}
+
+void PmergeMe::sortList() {
+    fordJohnsonSort(_listData);
+}
